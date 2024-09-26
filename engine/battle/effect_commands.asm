ebe741fd27acb4a78c3ef92d11798deeb15f9e69
LukasW <lukewilhelm94@gmail.com>
2024-09-25 21:25:51 -0400

    fixed TripleKick

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -5448,15 +5448,8 @@ BattleCommand_EndLoop:
 	jr z, .beat_up
 	cp EFFECT_TRIPLE_KICK
 	jr nz, .not_triple_kick
-.reject_triple_kick_sample
-	call BattleRandom
-	and $3
-	jr z, .reject_triple_kick_sample
-	dec a
-	jr nz, .double_hit
-	ld a, 1
-	ld [bc], a
-	jr .done_loop
+    ld a, 2
+    jr .double_hit
 
 .beat_up
 	ldh a, [hBattleTurn]
